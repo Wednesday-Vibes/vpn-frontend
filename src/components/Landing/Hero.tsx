f15687ce348c66264a4e80694fba938684311754
folabia1 <27552121+folabia1@users.noreply.github.com>
2023-03-09 05:58:19 +0000

    minor hero updates

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -7,18 +7,34 @@ const Hero = styled.div`
     .hero-floaty {
         position: absolute;
         top: 0;
-        width: 100%;
+        left: 0;
+        right: 0;
+        width: min(100%, 144rem);
         height: 100%;
+        margin: 0 auto;
+        padding: 3rem 2rem;
         display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        justify-content: center;
-        text-align: left;
-        gap: 2rem;
-        padding: 3rem;
-        .hero__buttons {
+        align-items: center;
+
+        .hero__content {
             display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: center;
+            text-align: left;
             gap: 2rem;
+            width: min(100%, 60rem);
+
+            .hero__text {
+                max-width: 70%;
+                h1 {
+                    margin-bottom: 1rem;
+                }
+            }
+            .hero__buttons {
+                display: flex;
+                gap: 2rem;
+            }
         }
     }
 `;
@@ -35,14 +51,18 @@ export default () => {
                 ]}
             />
             <div className="hero-floaty">
-                <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita deleniti nobis enim dignissimos, eum eveniet quod,
-                    quisquam voluptatibus sequi illo vel ea adipisci aliquam atque cum modi alias possimus delectus.
-                </p>
-                <div className="hero__buttons">
-                    <button>Large</button>
-                    <button className="button--secondary">Large</button>
+                <div className="hero__content">
+                    <div className="hero__text">
+                        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita deleniti nobis enim dignissimos, eum eveniet
+                            quod, quisquam voluptatibus sequi illo vel ea adipisci aliquam atque cum modi alias possimus delectus.
+                        </p>
+                    </div>
+                    <div className="hero__buttons">
+                        <button>Large</button>
+                        <button className="button--secondary">Large</button>
+                    </div>
                 </div>
             </div>
         </Hero>
