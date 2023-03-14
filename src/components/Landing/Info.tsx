4386ddb4236b3fb3b9efe9e90c3ca52c8f9cca51
folabia1 <27552121+folabia1@users.noreply.github.com>
2023-03-14 19:27:41 +0000

    attempt #1

--- src/components/Landing/Info.tsx
+++ src/components/Landing/Info.tsx
@@ -73,21 +73,21 @@ export default () => {
 
                 <div className="testimonials">
                     <Testimonial
-                        imageSrc="/pexels-mariatheodora-andrikopoulou-14390223.jpg"
+                        imageSrc="/vpn-frontend/pexels-mariatheodora-andrikopoulou-14390223.jpg"
                         title="Security: Encryption, No-Logging, Kill Switch"
                         textContent={
                             "Security is what matters most. That's why we use strong encryption to secure your data and protect it from hackers, government surveillance, and other online threats. Additionally, our no-logging policy ensures that your activity is not being monitored or recorded by the VPN provider. In case of a sudden VPN disconnection, a kill switch feature automatically cuts off internet access to prevent any data leaks."
                         }
                     />
                     <Testimonial
-                        imageSrc="/pexels-matt-hardy-5327027.jpg"
+                        imageSrc="/vpn-frontend/pexels-matt-hardy-5327027.jpg"
                         title="Privacy: Anonymous, Masking IP."
                         textContent={
                             'By masking your IP address, we keep your online activities anonymous and make it impossible for third parties to track your browsing history. Additional privacy features such as blocking tracking cookies or preventing browser fingerprinting ensure constant privacy.'
                         }
                     />
                     <Testimonial
-                        imageSrc="/pexels-mathilde-langevin-15823364.jpg"
+                        imageSrc="/vpn-frontend/pexels-mathilde-langevin-15823364.jpg"
                         title="Access: Geo-Unblocking, Lightning Fast Speeds, Torrent-Friendly."
                         textContent={
                             'We help you access content that may be restricted in your location, such as streaming services or websites. This is achieved by connecting to a server in a different country, which makes it appear as if you are browsing from that location. Fast speeds are paramount, so that you can count on us for streaming content without buffering or delays. Through this VPN service you can download and share files securely and anonymously.'
